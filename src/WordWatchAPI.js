9289956b16d01bbd432e981f17492626a419c6f2
William Thomas <wthoma390@gmail.com>
2017-09-05 10:33:59 -0600

    add api call to get top word

--- src/WordWatchAPI.js
+++ src/WordWatchAPI.js
@@ -1,2 +1,6 @@
 const $ = require('jquery')
 const base_url = 'https://wordwatch-api.herokuapp.com'
+
+class API {
+  constructor()
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ const $ = require('jquery')
 const WordWatch = require('./WordWatch')
 
 document.addEventListener("DOMContentLoaded", () => {
-  // have fun!
+
+  $(() => {
+    api.getTopWord()
+    .then((response) => {
+      const word = Object.keys(response.word)[0]
+      const value = Object.values(response.word)[0]
+      $('h3').replaceWith('<h3>Top word from Watch API:' + word + '(' + value + ')</h3>' )
+    })
+  })
 })
